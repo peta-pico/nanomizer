a8716aa58f1be1c7148f81ac725bf0cbe942c465
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2015-11-18 12:01:45 +0100

    Dataset parameters

--- scripts/prepare-data.sh
+++ scripts/prepare-data.sh
@@ -2,18 +2,28 @@
 #!/bin/bash
 
 # Prepares nanomizer data with Frank
+caterr() {
+    while IFS= read  line; do
+	echo "$line" 1>&2;
+    done
+}
 
-# show_help() {
-#     echo 'foo';
-# }
+show_help() {
+    caterr <<EOF
+Usage: ${0##*/} <minTriples> <maxTriples> <numDatasets> <path>
 
-# [ "$#" -eq 0 ] && show_help && exit;
+minTriples: minimum number of triples per dataset
+maxTriples: maximum number of triples per dataset
+numDatasets: number of datasets to download
+path: path to store the datasets
+EOF
+}
 
-echo $1
+[ "$#" -ne 4 ] && show_help && exit;
 
-for dlink in $(./Frank/frank documents --minTriples 500 --maxTriples 1000 | cut -f1 -d' ' | head -n100)
+for dlink in $(./Frank/frank documents --minTriples $1 --maxTriples $2 | cut -f1 -d' ' | head -n$3)
 do
-    curl $dlink -o ../data/${dlink##*/}.nt.gz;
+    curl $dlink -o $4/${dlink##*/}.nt.gz;
 done
 
 exit 0
