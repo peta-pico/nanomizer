481aaed8cc1fc4986f0bfab376514872e848b768
Tobias Kuhn <kuhntobias@gmail.com>
2015-11-18 11:10:30 +0100

    Build preliminary link map

--- src/main/java/org/petapico/nanomizer/Nanomize.java
+++ src/main/java/org/petapico/nanomizer/Nanomize.java
@@ -4,12 +4,18 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import javax.activation.MimetypesFileTypeMap;
 
 import org.nanopub.NanopubUtils;
 import org.openrdf.OpenRDFException;
+import org.openrdf.model.BNode;
 import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandlerException;
 import org.openrdf.rio.RDFParser;
@@ -29,6 +35,8 @@ public class Nanomize {
 
 	private File file;
 	private RDFFormat format;
+
+	private Map<String,Set<String>> linkMap = new HashMap<String, Set<String>>();
 
 	public Nanomize(File file) {
 		this.file = file;
@@ -50,6 +58,7 @@ public class Nanomize {
 		} finally {
 			in.close();
 		}
+		showResults();
 	}
 
 	private void processStream(InputStream in) throws OpenRDFException, IOException {
@@ -58,12 +67,24 @@ public class Nanomize {
 
 			@Override
 			public void handleStatement(Statement st) throws RDFHandlerException {
-				// TODO
-				System.out.println(st);
+				if (st.getSubject() instanceof BNode) {
+					throw new RuntimeException("Unexpected blank node encountered");
+				}
+				String s = st.getSubject().stringValue();
+				if (!(st.getObject() instanceof URI)) return;
+				String o = st.getObject().stringValue();
+				if (!linkMap.containsKey(o)) linkMap.put(o, new HashSet<String>());
+				linkMap.get(o).add(s);
 			}
 
 		});
 		p.parse(in, "");
+	}
+
+	private void showResults() {
+		for (String node : linkMap.keySet()) {
+			System.out.println(node + ": " + linkMap.get(node).size());
+		}
 	}
 
 }
