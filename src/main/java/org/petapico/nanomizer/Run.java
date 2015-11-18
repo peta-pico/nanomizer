1f9fd3d3cfd8226352382b1d620b7f59a35c65d5
Tobias Kuhn <kuhntobias@gmail.com>
2015-11-18 10:27:40 +0100

    Add scripts

--- src/main/java/org/petapico/nanomizer/Run.java
+++ src/main/java/org/petapico/nanomizer/Run.java
@@ -3,7 +3,7 @@ package org.petapico.nanomizer;
 public class Run {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Running Nanomizer...");
 	}
 
 }
