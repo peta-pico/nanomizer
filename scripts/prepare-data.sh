11373f69c7600aa58a075d096c167cc3878096e4
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2015-11-18 11:43:15 +0100

    Downloading datasets

--- scripts/prepare-data.sh
+++ scripts/prepare-data.sh
@@ -3,9 +3,15 @@
 
 # Prepares nanomizer data with Frank
 
-for dlink in $(./Frank/frank documents --minTriples 500 --maxTriples 1000 | cut -f1 -d' ' | head -n100)
+# show_help() {
+#     echo 'foo';
+# }
+
+# [ "$#" -eq 0 ] && show_help && exit;
+
+for dlink in $(./Frank/frank documents --minTriples 500 --maxTriples 1000 -g | cut -f1 -d' ' | head -n100)
 do
-    echo $dlink
+    curl $dlink -o ../data/${dlink##*/}.nt.gz;
 done
 
 exit 0
