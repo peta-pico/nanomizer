1212138048bb163286a33e73cc0551e0530c14ca
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2015-11-18 11:27:43 +0100

    Getting download links

--- scripts/prepare-data.sh
+++ scripts/prepare-data.sh
@@ -1,4 +1,11 @@
+
 #!/bin/bash
 
 # Prepares nanomizer data with Frank
 
+for dlink in $(./Frank/frank documents --minTriples 500 --maxTriples 1000 | cut -f1 -d' ' | head -n100)
+do
+    echo $dlink
+done
+
+exit 0
